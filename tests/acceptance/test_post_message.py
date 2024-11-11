72775c456f95c933ce9bde03a8131f95e57c91c7
christina.ripley <christina.ripley@mintecglobal.com>
2024-11-11 15:34:44 +0000

    add nonsense test

--- tests/acceptance/test_post_message.py
+++ tests/acceptance/test_post_message.py
@@ -1,3 +1,6 @@
 class TestPostMessage:
     def test_user_can_post_message_to_timeline(self):
+        assert True
+
+    def test_user_can_post_message_to_timeline_also(self):
         assert False
